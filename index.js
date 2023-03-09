409c5007447345f2ff7def195bf6723dcfb83127
chrisquim <chrisquim722@gmail.com>
2023-03-09 13:33:28 +0800

    2 files modified

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ import './style.css';
 import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
-import {getAuth, EmailAuthProvider} from 'firebase/auth';
-import {} from 'firebase/firestore';
+import {
+  getAuth,
+  EmailAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+} from 'firebase/firestore';
 
 import * as firebaseui from 'firebaseui';
 
@@ -28,15 +40,16 @@ let db, auth;
 async function main() {
   // Add Firebase project configuration object here
   const firebaseConfig = {
-    apiKey: "AIzaSyAuHYHlAvCBip2X-uRlqmWebpLggovyryk",
-    authDomain: "fir-web-codelab-94dec.firebaseapp.com",
-    projectId: "fir-web-codelab-94dec",
-    storageBucket: "fir-web-codelab-94dec.appspot.com",
-    messagingSenderId: "1036424088700",
-    appId: "1:1036424088700:web:2031215415b894ddf70df9"
+    apiKey: 'AIzaSyAuHYHlAvCBip2X-uRlqmWebpLggovyryk',
+    authDomain: 'fir-web-codelab-94dec.firebaseapp.com',
+    projectId: 'fir-web-codelab-94dec',
+    storageBucket: 'fir-web-codelab-94dec.appspot.com',
+    messagingSenderId: '1036424088700',
+    appId: '1:1036424088700:web:2031215415b894ddf70df9',
   };
-  // initializeApp(firebaseConfig);
-
+  initializeApp(firebaseConfig);
+  auth = getAuth();
+  db = getFirestore();
   // FirebaseUI config
   const uiConfig = {
     credentialHelper: firebaseui.auth.CredentialHelper.NONE,
@@ -53,6 +66,93 @@ async function main() {
     },
   };
 
-  // const ui = new firebaseui.auth.AuthUI(auth);
+  const ui = new firebaseui.auth.AuthUI(auth);
+  startRsvpButton.addEventListener('click', () => {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  });
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      startRsvpButton.textContent = 'LOGOUT';
+      guestbookContainer.style.display = 'block';
+    } else {
+      startRsvpButton.textContent = 'RSVP';
+      guestbookContainer.style.display = 'none';
+    }
+  });
+
+  form.addEventListener('submit', async (e) => {
+    // Prevent the default form redirect
+    e.preventDefault();
+    // Write a new message to the database collection "guestbook"
+    addDoc(collection(db, 'guestbook'), {
+      text: input.value,
+      timestamp: Date.now(),
+      name: auth.currentUser.displayName,
+      userId: auth.currentUser.uid,
+    });
+    // clear message input field
+    input.value = '';
+    // Return false to avoid redirect
+    return false;
+  });
+
+  // Create query for messages
+  const q = query(collection(db, 'guestbook'), orderBy('timestamp', 'desc'));
+  onSnapshot(q, (snaps) => {
+    // Reset page
+    guestbook.innerHTML = '';
+    // Loop through documents in database
+    snaps.forEach((doc) => {
+      // Create an HTML entry for each document and add it to the chat
+      const entry = document.createElement('p');
+      entry.textContent = doc.data().name + ': ' + doc.data().text;
+      guestbook.appendChild(entry);
+    });
+  });
 }
 main();
+
+// ...
+// Listen to guestbook updates
+function subscribeGuestbook() {
+  const q = query(collection(db, 'guestbook'), orderBy('timestamp', 'desc'));
+  guestbookListener = onSnapshot(q, (snaps) => {
+    // Reset page
+    guestbook.innerHTML = '';
+    // Loop through documents in database
+    snaps.forEach((doc) => {
+      // Create an HTML entry for each document and add it to the chat
+      const entry = document.createElement('p');
+      entry.textContent = doc.data().name + ': ' + doc.data().text;
+      guestbook.appendChild(entry);
+    });
+  });
+}
+
+// ...
+// Unsubscribe from guestbook updates
+function unsubscribeGuestbook() {
+  if (guestbookListener != null) {
+    guestbookListener();
+    guestbookListener = null;
+  }
+}
+
+// ...
+// Listen to the current Auth state
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    startRsvpButton.textContent = 'LOGOUT';
+    // Show guestbook to logged-in users
+    guestbookContainer.style.display = 'block';
+    // Subscribe to the guestbook collection
+    subscribeGuestbook();
+  } else {
+    startRsvpButton.textContent = 'RSVP';
+    // Hide guestbook for non-logged-in users
+    guestbookContainer.style.display = 'none';
+    // Unsubscribe from the guestbook collection
+    unsubscribeGuestbook();
+  }
+});
